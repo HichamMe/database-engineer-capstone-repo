af3f142ed42eb39304bb78e12d3617326075f330
HichamMe <83673634+HichamMe@users.noreply.github.com>
2024-01-26 21:19:02 +0100

    Add files via upload

--- CheckBooking.sql
+++ CheckBooking.sql
@@ -1,15 +1,21 @@
-DROP PROCEDURE IF EXISTS CheckBooking;
-
 DELIMITER //
-
-CREATE PROCEDURE CheckBooking(booking_date DATE, table_number INT)
+CREATE PROCEDURE IF NOT EXISTS CheckBooking (book_date DATE, book_time TIME, table_num INT)
 BEGIN
-
-SELECT CONCAT('Table ', table_number, ' is already booked') AS 'Booking status'
-FROM bookings
-WHERE BookingDate = booking_date 
-AND TableNumber = table_number;
-
-END //
-
+DECLARE table_booked INT DEFAULT 0;
+SELECT 
+    COUNT(table_booked)
+INTO table_booked FROM
+    bookings
+WHERE
+    booking_date = book_date
+        AND booking_time = book_time
+        AND table_no = table_num;
+    IF table_booked > 0 THEN
+      SELECT CONCAT( "Table ",table_num, " is already booked at ",book_time, " on ", book_date,".") AS "Booking status";
+      ELSE 
+      SELECT CONCAT( "Table ",table_num," is not booked at ",book_time," on ",book_date,".") AS "Booking status";
+    END IF;
+END//
 DELIMITER ;
+
+CALL CheckBooking("2022-10-10", "21:00:00", 5);
